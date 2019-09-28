6cc702bd6448f58dd9f08833ecfac6123eecec4a
amanpreetSingh97 <honeysingh.hs490@gmail.com>
2019-09-28 17:22:20 +0530

    final mul

--- mul.java
+++ mul.java
@@ -1,5 +1,5 @@
 public class mul {
 	public static void main(String []args) {
-		System.out.println(Integer.parseInt(args[0]) * Integer.pasreInt(args[1]));
+		System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]));
 	}
 }
